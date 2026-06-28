2ea775b534ebcdc50e61423a8d3f0eee94456b07
Ammar Iqbal <AmmarIqbal@protonmail.com>
2026-06-28 19:03:11 +0500

    feat: foundations task 6 completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (input, number) {
+    let output = String();
+    if (number < 0) {
+        return String("ERROR");
+    }
+    for (let i = 0; i < number; i++) {
+        output += String(input);
+    }
+    return output;
 };
 
 // Do not edit below this line
