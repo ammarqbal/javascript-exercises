5a8718a9f52049e3f2c84c7aff91ea1d285e47ff
Ammar Iqbal <AmmarIqbal@protonmail.com>
2026-07-01 20:06:39 +0500

    feat: task 7 completed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  if (input === "") {
+    return "";
+  }
+  let output = String("");
+  for (let i = input.length - 1; i >= 0; i--) {
+    output += input[i];
+  }
+  return output;
 };
 
 // Do not edit below this line
